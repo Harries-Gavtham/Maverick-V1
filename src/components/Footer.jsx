5cae37702e1257adbbcc0f382c2fe7786ad233b4
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-25 02:51:11 +0530

    Footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-teal-500 mt-3 rounded-xl py-4">
+      <div className="">
+        <p className="text-black font-bold text-center capitalize text-2xl">
+          Developed by <span className="uppercase">Harries Gavtham</span>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
