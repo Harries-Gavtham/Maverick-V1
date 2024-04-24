b3d50872df2c8c31bc5236b2a47b73de4f647cf3
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-25 00:01:14 +0530

    Hero component completed and is responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HeroImg from "../assets/hero.png";
+import HeroImg2 from "../assets/hero1.png";
 
 const Hero = () => {
   return (
     <section className=" mt-2">
-      <div className="bg-rose-200 rounded-xl h-[50vh] text-white">
-        <div className="">
-          <div className="">
-            <div className=""></div>New Trend
+      <div className="bg-rose-200 rounded-xl text-black w-full h-[400px]  py-1 px-3 sm:flex items-center justify-center">
+        <div className="flex flex-col justify-between items-center w-full gap-5 ">
+          <img src={HeroImg} className="w-[300px] sm:hidden" />
+          <div className="flex flex-col justify-center items-center gap-2 md:gap-4 lg:gap-6">
+            <p className="text-[1.4rem] md:text-[2rem] lg:text-[3rem] ">
+              New Trends
+            </p>
+            <h1 className="font-semibold text-2xl md:text-4xl lg:text-6xl">
+              SALE 50% OFF <br />
+            </h1>
+            <Link
+              to={"/"}
+              className="bg-black text-white px-4 py-2 text-xl rounded-md hover:text-gray-400 cursor-pointer md:text-3xl lg:text-4xl"
+            >
+              Discover More
+            </Link>
           </div>
-          <h1 className="">
-            AUTUMN SALE STYLISH <br />
-            <span className="font-semibold">WOMENS</span>
-          </h1>
-          <Link to={"/"} className="">
-            Discover More
-          </Link>
         </div>
-        <div className="">
-          <img src="" />
+        <div className="hidden sm:block">
+          <img src={HeroImg2} className="w-[1000px]" />
         </div>
       </div>
     </section>
