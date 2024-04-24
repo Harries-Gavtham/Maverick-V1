6970661b6a2b695648d89052fa58825d3e326f73
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-24 18:56:45 +0530

    App is set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/product/:id" element={<ProductDetails />}></Route>
+        </Routes>
+        <SideBar />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
