efd24eff319113f43e9265c6d37c586781e799df
Harries-Gavtham <harriesjj77@gmail.com>
2024-04-24 20:16:44 +0530

    Hero Component on progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className=" mt-2">
+      <div className="bg-rose-200 rounded-xl h-[50vh] text-white">
+        <div className="">
+          <div className="">
+            <div className=""></div>New Trend
+          </div>
+          <h1 className="">
+            AUTUMN SALE STYLISH <br />
+            <span className="font-semibold">WOMENS</span>
+          </h1>
+          <Link to={"/"} className="">
+            Discover More
+          </Link>
+        </div>
+        <div className="">
+          <img src="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
